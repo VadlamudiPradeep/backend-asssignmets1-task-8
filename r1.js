353a6e2a17b119773a0d2830943e8547827823c4
vadlamudi pradeep <vandlamudipradeepchoudhary@gmail.com>
2022-11-17 22:13:40 +0530

    first commit

--- r1.js
+++ r1.js
@@ -42,6 +42,14 @@ const requestHandler = (req, res) => {
   // res.end();
 };
 
+// module.exports = requestHandler;
 
+// module.exports = {
+//     handler: requestHandler,
+//     someText: 'Some hard coded text'
+// };
+
+// module.exports.handler = requestHandler;
+// module.exports.someText = 'Some text';
 exports.handler = requestHandler;
 exports.someText = 'Some hard coded text';
